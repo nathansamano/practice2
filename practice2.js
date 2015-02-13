b591e288f836d6acc04bf85abb573519342c635b
Nathan Samano <nsv9982@saintjoe.edu>
2015-02-13 12:50:23 -0500

    completed exercise

---
+++ practice2.js
@@ -0,0 +1,9 @@
+/* Nathan Samano
+ * Type a string - at least 10 mixed case
+ * print out the 4th 7th 10th
+ * turn it all uppercase, or all lower
+ */
+
+var string = "soegCkalurR";
+
+print(string[4].toLowerCase() + string[6].toLowerCase() + string[10].toLowerCase());
